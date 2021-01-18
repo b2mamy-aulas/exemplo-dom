132c6315e03987c443eb505664a7aaea07582d29
diandra <di.andrade90@gmail.com>
2021-01-17 21:51:31 -0300

    Primeira estrutura do site

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    'title':'CodeAcademy',
+    'url':'https://www.codecademy.com/',
+    'description':'Aprenda skills técnicas para o trabalho que você procura',
+    'image': 'images/image01.jpg',
+    'tags': ['online','gringo']
+  }
+]
